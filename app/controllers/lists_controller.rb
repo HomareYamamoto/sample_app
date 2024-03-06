3a9f6247d969b12108ccaa8e16f8034e7ff4e0cf
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-03-06 08:38:39 +0000

    [finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,22 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
 
+
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+
   def index
+    @lists=List.all
   end
 
   def show
@@ -10,4 +24,12 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+
+  private #privateは一種の境界線で、「ここから下はこのcontrollerの中でしか呼び出せません」という意味があります。
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
